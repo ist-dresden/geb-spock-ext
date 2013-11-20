d66db018020913d601ea50a7bf65c49b5fbb2c47
Ralf Wunsch <r.wunsch@ist-software.com>
2013-11-20 16:30:41 +0100

    initial code fragments

--- xmpl/src/main/java/test/ExamplesSuite.java
+++ xmpl/src/main/java/test/ExamplesSuite.java
@@ -2,9 +2,6 @@ package test;
 
 import junit.framework.TestSuite;
 import org.junit.runners.Suite;
-import test.example.Achtgrau;
-import test.example.IstSite;
-import test.example.LocalCQ;
 import test.example.TestcaseOne;
 import test.example.TestcaseTwo;
 
